ca4a85f95ade7c1df387553a33e73dc3f844621f
Cierra_Runis <2864283875@qq.com>
2023-11-01 14:58:39 +0800

    add CommentComponent

--- volo/src/app/page.tsx
+++ volo/src/app/page.tsx
@@ -1,5 +1,25 @@
-import { UploadTest } from "./_components/upload-test";
+import { db } from "~/server/db";
+import CommentComponent from "./_components/comment";
 
-export default function Home() {
-  return <UploadTest />;
+export default async function Home() {
+  const comments = await db.comment.findMany({
+    select: {
+      id: true,
+      text: true,
+      imgUrl: true,
+      likes: true,
+      dislikes: true,
+      createdAt: true,
+      author: {
+        select: {
+          id: true,
+          name: true,
+          avatarUrl: true,
+        },
+      },
+    },
+  });
+  return comments.map((comment) => {
+    <CommentComponent comment={comment} />;
+  });
 }
