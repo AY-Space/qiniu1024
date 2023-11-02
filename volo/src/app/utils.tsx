faeea6409c7cc7fe2bd8ce251b466e7090876039
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2023-11-02 16:03:30 +0800

    utils: use breakpoint

--- volo/src/app/utils.tsx
+++ volo/src/app/utils.tsx
@@ -1,4 +1,14 @@
+import { type Theme, useTheme } from "@mui/joy";
+import { useMediaQuery } from "usehooks-ts";
+
 export const getBilibiliImageUrl = (srcUrl: string) =>
   `http://localhost:3080/bilibili-image?${new URLSearchParams({
     url: srcUrl.replace("http://", "https://"),
   }).toString()}`;
+
+export const useBreakpointUp = (
+  breakpoint: Theme["breakpoints"]["keys"][number],
+) => {
+  const theme = useTheme();
+  return useMediaQuery(theme.breakpoints.up(breakpoint));
+};
