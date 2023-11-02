9426faf65d204436d0a51ae039a391383da9c139
Cierra_Runis <2864283875@qq.com>
2023-11-02 22:25:40 +0800

    improve comment drawer

--- volo/src/app/video/[id]/_components/comment-drawer.tsx
+++ volo/src/app/video/[id]/_components/comment-drawer.tsx
@@ -13,6 +13,8 @@ import {
   Alert,
   CircularProgress,
   Container,
+  DialogTitle,
+  Divider,
 } from "@mui/joy";
 import Image from "next/image";
 import { getBilibiliImageUrl } from "../../../utils";
@@ -25,13 +27,13 @@ import { api } from "~/trpc/react";
 
 const Comment = ({ comment }: { comment: CommentPublic }) => {
   return (
-    <Stack direction="row" gap={1}>
+    <Stack direction="row" gap={1} paddingY={2}>
       {comment.author.avatarUrl && (
         <Avatar src={getBilibiliImageUrl(comment.author.avatarUrl)} />
       )}
       <Stack spacing={1}>
         <Stack>
-          <Typography level="title-sm">{comment.author.name}</Typography>
+          <Typography level="title-lg">{comment.author.name}</Typography>
           <Typography level="body-xs">
             {dayjs(comment.createdAt).fromNow()}
           </Typography>
@@ -65,7 +67,7 @@ const Comment = ({ comment }: { comment: CommentPublic }) => {
 const CommentList = ({ videoId }: { videoId: string }) => {
   const { data, error, isLoading } = api.video.comments.useQuery({ videoId });
   return (
-    <Stack spacing={2}>
+    <Stack>
       {error && <Alert color="danger">{error.message}</Alert>}
       {isLoading && <CircularProgress />}
       {data?.map((comment) => <Comment comment={comment} key={comment.id} />)}
@@ -86,8 +88,10 @@ export const CommentDrawer = ({
   console.log(videoId, open);
   return (
     <Drawer open={open} onClose={onClose} anchor="right">
+      <DialogTitle>评论</DialogTitle>
       <ModalClose />
-      <Container sx={{ marginTop: 8 }}>
+      <Container>
+        <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
         <CommentList videoId={videoId} />
       </Container>
     </Drawer>
