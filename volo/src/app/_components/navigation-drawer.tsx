07d177d7bbcce187519e9c7c2d1cfa8051a717af
Cierra_Runis <2864283875@qq.com>
2023-11-04 23:28:33 +0800

    📖Add category selector to NavigationDrawer

--- volo/src/app/_components/navigation-drawer.tsx
+++ volo/src/app/_components/navigation-drawer.tsx
@@ -1,35 +1,61 @@
+"use client";
+
 import {
-  Box,
   Divider,
   Drawer,
   type DrawerProps,
   List,
-  ListItem,
-  ListItemButton,
+  Typography,
+  DialogTitle,
+  Card,
+  Grid,
+  Radio,
+  DialogContent,
 } from "@mui/joy";
+import { useState } from "react";
+import { api } from "~/trpc/react";
 
 export type NavigationDrawer = Pick<DrawerProps, "open" | "onClose">;
 
 export function NavigationDrawer({ open, onClose }: NavigationDrawer) {
+  const { data: categories } = api.tag.categories.useQuery();
+  const [selectedCategoryId, setSelectedCategoryId] = useState<string | null>(
+    null,
+  );
+
   return (
     <Drawer open={open} onClose={onClose}>
-      <Box role="presentation">
+      <DialogTitle>Title</DialogTitle>
+      <Divider />
+      <DialogContent sx={{ p: 1.3 }}>
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text) => (
-            <ListItem key={text}>
-              <ListItemButton>{text}</ListItemButton>
-            </ListItem>
-          ))}
+          <Typography level="title-lg">分类</Typography>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            p={2}
+          >
+            {categories?.map((category) => (
+              <Grid key={category.id} xs={6}>
+                <Card>
+                  <Radio
+                    overlay
+                    checked={category.id === selectedCategoryId}
+                    label={category.name}
+                    sx={{ flexGrow: 1, flexDirection: "row-reverse" }}
+                    onClick={() =>
+                      setSelectedCategoryId(
+                        category.id === selectedCategoryId ? null : category.id,
+                      )
+                    }
+                  />
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text) => (
-            <ListItem key={text}>
-              <ListItemButton>{text}</ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
+      </DialogContent>
     </Drawer>
   );
 }
