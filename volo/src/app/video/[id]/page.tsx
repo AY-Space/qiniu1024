a5f29b9340a0e4c9f97d9847b44483b68e44bd03
ComfyFluffy <24245520+ComfyFluffy@users.noreply.github.com>
2023-11-05 11:30:01 +0800

    fix ssr error & first video

--- volo/src/app/video/[id]/page.tsx
+++ volo/src/app/video/[id]/page.tsx
@@ -1,14 +1,22 @@
 import { Stack } from "@mui/joy";
 import { db } from "~/server/db";
 import { VideoContainer } from "./_components/video-container";
+import { notFound } from "next/navigation";
+import { getVideos } from "~/server/lib/db/video";
+import { getServerAuthSession } from "~/server/auth";
 
-export default function Video({
+export default async function Video({
   params: { id },
 }: {
   params: {
     id: string;
   };
 }) {
+  const session = await getServerAuthSession();
+  const video = await getVideos(db, [id], session?.userId);
+  if (video.length === 0) {
+    notFound();
+  }
   return (
     <Stack
       sx={{
@@ -16,7 +24,7 @@ export default function Video({
         overflow: "hidden",
       }}
     >
-      <VideoContainer />
+      <VideoContainer initalVideo={video[0]} />
     </Stack>
   );
 }
