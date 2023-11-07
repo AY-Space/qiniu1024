9895aea8044e5b257ba5811016d029ab633c331d
AY7295 <Adolph7295@outlook.com>
2023-11-07 11:08:14 +0800

    deploy: let app tabke PID 1

--- volo/app.sh
+++ volo/app.sh
@@ -5,9 +5,9 @@ echo "init db tables..."
 pnpm db:push
 echo "init db tables done"
 
-if [ -n "$INIDB" ]; then
+if [ -n "$INIT_DB" ]; then
   pnpm scripts scripts/init.js
 fi
 
 echo "starting app"
-pnpm dev
+exec pnpm dev
